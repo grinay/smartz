ec4425e3777deb6aa1fba07aafd43d9804c5fb58
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2017-12-16 09:44:14 +0300

    Auth with auth0 service bootstrapped

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {makeMainRoutes} from './routes';
+import './index.css';
+
+const routes = makeMainRoutes();
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
