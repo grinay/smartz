0787042610335526f8187a74cc3c2594baead9b5
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2017-12-16 16:45:13 +0300

    Add ctor form update

--- front/src/CtorAdd/CtorAdd.js
+++ front/src/CtorAdd/CtorAdd.js
@@ -4,16 +4,6 @@ import axios from 'axios';
 
 import {API_URL} from '../constants';
 
-function FieldGroup({ id, label, help, ...props }) {
-  return (
-    <FormGroup controlId={id}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
-    </FormGroup>
-  );
-}
-
 class CtorAdd extends Component {
   constructor(props) {
     super(props);
@@ -25,7 +15,7 @@ class CtorAdd extends Component {
   submit() {
     axios.post(`${API_URL}/upload_ctor`, {
       'ctor_name': this.state.name,
-      'ctor_file_name': '1'
+      'ctor_file_name': this.state.file
     })
       .then(response => console.log(response.data.message))
       .catch(error => console.log(error));
@@ -50,14 +40,17 @@ class CtorAdd extends Component {
               onChange={this.handleChange.bind(this)}
             />
           </FormGroup>
-          <FieldGroup
-            id="formControlsFile"
-            type="file"
-            label="File"
-            name="file"
-            help="Example block-level help text here."
-            onChange={this.handleChange.bind(this)}
-          />
+          <FormGroup
+            controlId="formBasicText"
+          >
+            <FormControl
+              name="file"
+              type="text"
+              value={this.state.file}
+              placeholder="Enter file name (or what?)"
+              onChange={this.handleChange.bind(this)}
+            />
+          </FormGroup>
           <Button
             bsStyle="primary"
             className="btn-margin"
