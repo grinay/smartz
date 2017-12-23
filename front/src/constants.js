bcd0f7e9f39dc105f119c1fac76b1ef7217401e3
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2017-12-23 17:50:03 +0300

    API address bug fixed + build

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://smart.mixbytes.io/prod';
+export const API_URL = /localhost/.test(window.location.origin) ? 'http://smart.mixbytes.io/prod' : window.location.origin + '/prod';
