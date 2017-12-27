7729c6c1e3b7b6e76883afa9ed865a7949c25b5e
Mikhail Shubin <mikhail.shubin@gmail.com>
2017-12-27 15:06:25 +0300

    workable ver

---
+++ docker/backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+chown -R uwsgi:uwsgi /app
+
+ls -lad /app/sock
+
+/usr/sbin/uwsgi \
+--uid uwsgi --gid uwsgi \
+--socket /app/sock/backend \
+--master \
+--plugin python3 \
+--virtualenv .virtualenv \
+--pythonpath /app/bin \
+--mount /backend=wsgi_app:app \
+--die-on-term \
+--processes 4
