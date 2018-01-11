0a95f1e3a695ff71090ec048e567887ed2c274f7
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-01-11 18:08:27 +0300

    GOSS_SLEEP=30

--- test/tests.sh
+++ test/tests.sh
@@ -2,17 +2,16 @@
 set -ex
 
 SOCKET_UID="10000"
-GOSS_SLEEP=10
 
 # frontend tests
 sudo docker build -f docker/frontend/Dockerfile -t frontend:testing --build-arg NGINX_UID="$SOCKET_UID" .
 sed -i "s/uid:\s.*$/uid: $SOCKET_UID/" docker/frontend/goss.yaml
-GOSS_FILES_PATH=docker/frontend dgoss run frontend:testing
+GOSS_SLEEP=30 GOSS_FILES_PATH=docker/frontend dgoss run frontend:testing
 
 # backend tests
 sudo docker build -f docker/backend/Dockerfile -t backend:testing --build-arg UWSGI_UID="$SOCKET_UID" .
 sed -i "s/uid:\s.*$/uid: $SOCKET_UID/" docker/backend/goss.yaml
-GOSS_FILES_PATH=docker/backend dgoss run backend:testing
+GOSS_SLEEP=30 GOSS_FILES_PATH=docker/backend dgoss run backend:testing
 
 # integration tests
 sudo docker-compose build
