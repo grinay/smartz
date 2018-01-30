c4898bd27562c7346adb692fd038b1b13acd4c9a
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-01-30 16:48:03 +0300

    buildinfo

--- docker/buildinfo.sh
+++ docker/buildinfo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo -e "Travis-CI build number: $TRAVIS_BUILD_NUMBER\nCommit: $TRAVIS_COMMIT\nCommit message: $TRAVIS_COMMIT_MESSAGE" > front/buildinfo
+echo -e "Travis-CI build number: $TRAVIS_BUILD_NUMBER\nCommit: $TRAVIS_COMMIT\nCommit message: $TRAVIS_COMMIT_MESSAGE" > front/buildinfo.txt
 
