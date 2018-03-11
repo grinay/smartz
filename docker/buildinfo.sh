387191f0f5eec78ec0b8bc1661fdd2afaceaa96b
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-03-11 20:46:06 +0300

    service files refactored

--- docker/buildinfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Travis-CI build<br>\n" > front/buildinfo.txt
-echo "Build number: $TRAVIS_BUILD_NUMBER<br>\n" >> front/buildinfo.txt
-echo "Branch: $TRAVIS_BRANCH<br>\n" >> front/buildinfo.txt
-echo "Commit: $TRAVIS_COMMIT<br>\n" >> front/buildinfo.txt
-echo "Commit message: $TRAVIS_COMMIT_MESSAGE<br>" >> front/buildinfo.txt
