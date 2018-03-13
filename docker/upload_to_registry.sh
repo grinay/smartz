cd9a716eef6579a5c90afba98f07b6218f189936
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-03-13 12:51:37 +0300

    tag docker images with branch and commit - fixed

--- docker/upload_to_registry.sh
+++ docker/upload_to_registry.sh
@@ -7,14 +7,13 @@ AWS_REGISTRY=128894178443.dkr.ecr.eu-central-1.amazonaws.com
 AWS_REGION=eu-central-1
 BRANCH="$TRAVIS_BRANCH"
 COMMIT="$(git log -1  --pretty=format:'%h')"
-TAGS=( "latest" "branch_$BRANCH_commit_$COMMIT" )
+TAGS=( "latest" "branch_${BRANCH}_commit_${COMMIT}" )
 
 for NAME in "$@"
 do
 	for TAG in "${TAGS[@]}"
 	do
-		echo $TAG
-		docker tag $NAME:latest $AWS_REGISTRY/$NAME:$TAG
-		ecs-cli push --region $AWS_REGION --ecs-profile travis $AWS_REGISTRY/$NAME:$TAG
+		docker tag "$NAME:latest" "$AWS_REGISTRY/$NAME:$TAG"
+		ecs-cli push --region "$AWS_REGION" --ecs-profile travis "$AWS_REGISTRY/$NAME:$TAG"
 	done
 done
