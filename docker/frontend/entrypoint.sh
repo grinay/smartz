d54fac90db2cf113536a2b76d9c80d1b53587852
Mikhail Shubin <mikhail.shubin@gmail.com>
2018-03-15 16:51:21 +0300

    fix robots.txt

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -8,12 +8,12 @@ case "$ENVIRONMENT" in
         stage)
                 echo "environment: STAGE"
                 ln -sf /app/conf/nginx.conf-prod /etc/nginx/nginx.conf
-                ln -sf /app/conf/robots.txt-stage /app/robots.txt
+                ln -sf /app/conf/robots.txt-stage /app/web_root/robots.txt
                 ;;
         prod)
                 echo "  environment: PRODUCTION"
                 ln -sf /app/conf/nginx.conf-prod /etc/nginx/nginx.conf
-                ln -sf /app/conf/robots.txt-prod /app/robots.txt
+                ln -sf /app/conf/robots.txt-prod /app/web_root/robots.txt
                 ;;
         *)
                 echo "Please define environment with ENVIRONMENT variable(dev, stage, prod)!"
