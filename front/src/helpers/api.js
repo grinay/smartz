e0f7dff6d28b908a51e14684e1539ef213d311b0
SlMel <jefferi@ya.ru>
2018-04-13 12:36:57 +0300

    SD-111 Add redirect url (only exists route)

--- front/src/helpers/api.js
+++ front/src/helpers/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API_URL = /localhost/.test(window.location.origin)
-  ? 'http://localhost:3000/api'
+  // ? 'http://localhost:3000/api'
+  ? 'http://172.28.128.3:3000/api'
   : window.location.origin + '/api';
 
 const api = (auth) => {
