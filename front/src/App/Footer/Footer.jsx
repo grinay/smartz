be05d145a368aba63bbc604c6c70720f997ed1bc
SlMel <jefferi@ya.ru>
2018-05-17 17:55:23 +0300

    SD-186 Change about link

--- front/src/App/Footer/Footer.jsx
+++ front/src/App/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Footer.css';
 
@@ -12,10 +12,10 @@ class Footer extends Component {
             SEO header
           </h2>
           <p className="page-footer__logo">
-              <img
-                  src={require('./i/logo.svg')}
-                  alt="Logo"
-              />
+            <img
+              src={require('./i/logo.svg')}
+              alt="Logo"
+            />
           </p>
           <div className="page-footer__block">
             <div className="page-footer__section  page-footer__section--store">
@@ -102,42 +102,42 @@ class Footer extends Component {
               </h3>
               <ul className="page-footer__list">
                 <li className="page-footer__item">
-                  <a href="https://platform.smartz.io/" className="page-footer__link" target="_blank" rel="noopener noreferrer">
+                  <a href="https://about.smartz.io/" className="page-footer__link" target="_blank" rel="noopener noreferrer">
                     About Smartz
                   </a>
                 </li>
                 <li className="page-footer__item">
                   <ul className="social">
                     <li className="social__item">
-                      <a href="https://www.facebook.com/SmartzPlatform/" className="social__link  social__link--facebook" aria-label="Follow us on Facebook"  target="_blank"  rel="noopener noreferrer">
+                      <a href="https://www.facebook.com/SmartzPlatform/" className="social__link  social__link--facebook" aria-label="Follow us on Facebook" target="_blank" rel="noopener noreferrer">
                         <svg className="social__icon  social__icon--facebook" width="26" height="26">
                           <use href="#social-fb"></use>
                         </svg>
                       </a>
                     </li>
                     <li className="social__item">
-                      <a href="https://twitter.com/SmartzPlatform" className="social__link  social__link--twitter" aria-label="Follow us on Twitter"  target="_blank"  rel="noopener noreferrer">
+                      <a href="https://twitter.com/SmartzPlatform" className="social__link  social__link--twitter" aria-label="Follow us on Twitter" target="_blank" rel="noopener noreferrer">
                         <svg className="social__icon  social__icon--twitter" width="27" height="22">
                           <use href="#social-twitter"></use>
                         </svg>
                       </a>
                     </li>
                     <li className="social__item">
-                      <a href="https://github.com/mixbytes" className="social__link  social__link--github" aria-label="Follow us on Github"  target="_blank"  rel="noopener noreferrer">
+                      <a href="https://github.com/mixbytes" className="social__link  social__link--github" aria-label="Follow us on Github" target="_blank" rel="noopener noreferrer">
                         <svg className="social__icon  social__icon--github" width="26" height="26">
                           <use href="#social-github"></use>
                         </svg>
                       </a>
                     </li>
                     <li className="social__item">
-                      <a href="https://www.youtube.com/channel/UCn4kywh7NwNNHce2a2murSg" className="social__link  social__link--youtube" aria-label="Follow us on Youtube"  target="_blank"  rel="noopener noreferrer">
+                      <a href="https://www.youtube.com/channel/UCn4kywh7NwNNHce2a2murSg" className="social__link  social__link--youtube" aria-label="Follow us on Youtube" target="_blank" rel="noopener noreferrer">
                         <svg className="social__icon  social__icon--youtube" width="26" height="18">
                           <use href="#social-youtube"></use>
                         </svg>
                       </a>
                     </li>
                     <li className="social__item">
-                      <a href="https://t.me/smartz_en" className="social__link  social__link--telegram" aria-label="Follow us on Telegram"  target="_blank"  rel="noopener noreferrer">
+                      <a href="https://t.me/smartz_en" className="social__link  social__link--telegram" aria-label="Follow us on Telegram" target="_blank" rel="noopener noreferrer">
                         <svg className="social__icon  social__icon--telegram" width="23" height="20">
                           <use href="#social-telegram"></use>
                         </svg>
@@ -151,7 +151,7 @@ class Footer extends Component {
                   </a>
                 </li>
                 <li className="page-footer__item">
-                  <a href="https://t.me/LoungerX" className="page-footer__link" target="_blank"  rel="noopener noreferrer">
+                  <a href="https://t.me/LoungerX" className="page-footer__link" target="_blank" rel="noopener noreferrer">
                     t.me/LoungerX
                   </a>
                 </li>
@@ -173,7 +173,7 @@ class Footer extends Component {
             <a href="/legal/Smartz_terms-and-conditions.pdf">Terms & conditions</a>
           </p>
           <p className="page-footer__copyright">
-            <span>&#169; Smartz, </span><a href="https://mixbytes.io" target="_blank"  rel="noopener noreferrer">MixBytes</a> <span>2018</span>
+            <span>&#169; Smartz, </span><a href="https://mixbytes.io" target="_blank" rel="noopener noreferrer">MixBytes</a> <span>2018</span>
           </p>
         </section>
       </footer>
