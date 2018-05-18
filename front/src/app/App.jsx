aa1724e8dcc10707154c9bc3147e83af234d8279
SlMel <jefferi@ya.ru>
2018-05-18 19:50:37 +0300

    SD-145 Fix case sens folder

---
+++ front/src/app/App.jsx
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import Auth from './auth/Auth';
+import Callback from './auth/callback/Callback';
+import Store from './store/StoreContainer';
+import MyDapps from './my-dapps/MyDappsContainer';
+import Profile from './profile/Profile';
+import Deploy from './deploy/DeployContainer';
+import CtorAdd from './ctor-add/CtorAdd';
+import Dashboard from './dashboard/DashboardContainer';
+import Instance from './instances/InstanceContainer';
+import Docs from './docs/Docs';
+import InfoBlock from './common/InfoBlock';
+import metamask from './common/img/metamask.png';
+import { checkMetaMask } from '../helpers/eth';
+
+import './App.less';
+
+const handleAuthentication = ({ location }) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    Auth.handleAuthentication();
+  }
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => {
+      if (Auth.isAuthenticated()) {
+        return <Component {...props} />;
+      } else {
+        Auth.login(window.location.pathname);
+
+        return <Callback {...props} />;
+      }
+    }}
+  />
+);
+
+class App extends Component {
+  componentWillMount() {
+    this.setState({});
+
+    let metamaskStatus = false;
+    
+    setInterval(() => {
+      if (metamaskStatus !== checkMetaMask()) {
+        metamaskStatus = checkMetaMask();
+        this.setState({ metamaskStatus });
+      }
+    }, 250);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    const { metamaskStatus } = this.state;
+    const isAuthenticated = Auth.isAuthenticated();
+    const { profile, setUserProfile } = this.props;
+
+    if (!isAuthenticated && profile) setUserProfile(null);
+    if (isAuthenticated && !profile) {
+      Auth.getProfile((err, newProfile) => {
+        setUserProfile(newProfile);
+      });
+    }
+
+    return (
+      <div>
+        {/* block 'Install metamask' */}
+        {metamaskStatus === 'noMetamask' &&
+          <InfoBlock className="install-block flex">
+            <img src={metamask} alt="" />
+            <p>To pay Ether you need a Metamask plugin.</p>
+            <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
+            target="_blanc">
+            <button>Install for Chrome</button>
+            </a>
+          </InfoBlock>
+        }
+
+        <Route render={(props) => (
+          <Header profile={profile} {...props} />
+        )} />
+
+        <Switch>
+          <Route exact path="/" render={(props) => (
+            <Store metamaskStatus={metamaskStatus} {...props} />
+          )} />
+          <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} />
+          }} />
+          <Route path="/docs/:docUri?" component={Docs} />
+
+          <PrivateRoute path="/profile" component={props =>
+            <Profile profile={profile} {...props} />}
+          />
+
+          <PrivateRoute exact path="/deploy/:ctorId" component={props =>
+            <Redirect to={`/deploy/${props.match.params.ctorId}/${this.props.nextDeploy}`} />
+          } />
+
+          <PrivateRoute path="/deploy/:ctorId/:deployId" component={props =>
+            <Deploy metamaskStatus={metamaskStatus} {...props} />
+          } />
+
+          <PrivateRoute path="/dashboard" component={props =>
+            <Dashboard metamaskStatus={metamaskStatus} {...props} />
+          } />
+          <PrivateRoute path="/instance/:id" component={props =>
+            <Instance metamaskStatus={metamaskStatus} {...props} />
+          } />
+
+          <PrivateRoute path="/ctor-add" component={props =>
+            <CtorAdd {...props} />
+          } />
+
+          <PrivateRoute path="/constructors/:id/update" component={props =>
+            <CtorAdd {...props} />
+          } />
+
+          <PrivateRoute path="/my-dapps" component={props =>
+            <MyDapps metamaskStatus={metamaskStatus} {...props} />
+          } />
+
+          {/* TODO: <Route component={Page404} />*/}
+        </Switch>
+
+        <Route render={(props) => <Footer {...props} />} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front/src/app/auth/Auth.js
@@ -0,0 +1,98 @@
+import auth0 from 'auth0-js';
+
+import { authConfig } from '../../../config/config';
+import history from '../../helpers/history';
+
+class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: authConfig.DOMAIN,
+    clientID: authConfig.CLIENT_ID,
+    redirectUri: authConfig.CALLBACK_URL,
+    audience: `https://${authConfig.DOMAIN}/userinfo`,
+    responseType: 'token id_token',
+    scope: 'openid profile'
+  });
+
+  userProfile;
+
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+    this.getProfile = this.getProfile.bind(this);
+  }
+
+  login(redirectUri = '/') {
+    // set redirect route after login
+    localStorage.setItem('route_after_login', redirectUri);
+    this.auth0.authorize();
+  }
+
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if (err) {
+        history.replace('/');
+        console.log(err);
+        // alert(`Error: ${err.error}. Check the console for further details.`);
+      }
+    });
+  }
+
+  setSession(authResult) {
+    // Set the time that the access token will expire at
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the redirect route
+    history.replace(localStorage.getItem('route_after_login'));
+  }
+
+  getAccessToken() {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+    return accessToken;
+  }
+
+  getProfile(cb) {
+    let accessToken = this.getAccessToken();
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        this.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    this.userProfile = null;
+    // navigate to the home route
+    history.replace('/');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
+
+// Singleton.
+// A solution with ES6 is to use an instance of a class scoped to a module.
+// There are some drawbacks though:
+// if you want to use a static method, you will have 
+// to use the constructor property of the exported instance.
+export default new Auth();
