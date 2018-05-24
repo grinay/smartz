a3765613bfb53a53c00c9065cc7c1d35a49a3678
SlMel <jefferi@ya.ru>
2018-05-24 19:54:29 +0300

    SD-202 Add footer

--- front/src/app/header/Header.jsx
+++ front/src/app/header/Header.jsx
@@ -111,50 +111,6 @@ class Header extends Component {
             </Link>
           </ul>
         </nav>
-        {/* <div className="user-block">
-        </div> */}
-        {/*
-      <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <div className="logo">
-              <Navbar.Brand>
-                <Link to="/">
-                  <img src={require('./i/smartz-logo.jpg')} alt="Smartz logo" />
-                </Link>
-                <span className="version">v0.4.1 alpha</span>
-              </Navbar.Brand>
-            </div>
-
-            <div className="nav-buttons">
-              {!isAuthenticated && (
-                <Button
-                  bsStyle="primary"
-                  className="btn-margin"
-                  onClick={Auth.login}
-                >
-                  Log In
-                </Button>
-              )}
-
-              {isAuthenticated && (
-                <span>
-                  <Link to={'/dashboard'} className="btn btn-primary btn-margin">
-                    My contracts
-                  </Link>
-                  <Link to={'/profile'} className="btn btn-primary btn-margin">
-                    Profile
-                  </Link>
-                  <Button className="btn btn-primary btn-margin" onClick={Auth.logout}>
-                    Log Out
-                  </Button>
-                </span>
-              )}
-            </div>
-          </Navbar.Header>
-        </Navbar>
-      </div>
-      */}
       </header>
     );
   }
