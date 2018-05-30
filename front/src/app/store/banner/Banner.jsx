501b61dc771078ef6bce3e2704ce1222aaa0c05e
SlMel <jefferi@ya.ru>
2018-05-30 16:29:57 +0300

    SD-174 Fix banner link click

--- front/src/app/store/banner/Banner.jsx
+++ front/src/app/store/banner/Banner.jsx
@@ -6,17 +6,17 @@ import './Banner.less';
 export default class Banner extends PureComponent {
     render() {
         return (
-            <div className="banner-telegram flex-v">
+            <a
+                href="https://t.me/smartz_en"
+                target="_blanc"
+                className="banner-telegram flex-v">
                 <p className="description">Get the latest news, communicate with like-minded people and ask questions.</p>
-                <a
-                    href="https://t.me/smartz_en"
-                    target="_blanc"
-                    className="chat-link">Join chat</a>
+                <p className="chat-link">Join chat</p>
                 <div className="bg-wrapper flex-v">
                     <div className="bg" />
                 </div>
                 <img src={tlgImg} alt="telegram" className="tlg" />
-            </div>
+            </a>
         );
     }
 };
