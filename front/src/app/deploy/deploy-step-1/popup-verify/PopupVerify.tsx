3eb0f0c9df216f314c162321e3d069dcb23eb9e3
SlMel <jefferi@ya.ru>
2018-06-29 12:44:27 +0300

    SD-264 Add Popup verify

---
+++ front/src/app/deploy/deploy-step-1/popup-verify/PopupVerify.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import './PopupVerify.less';
+
+
+export default class PopupVerify extends React.PureComponent<{}, {}> {
+  public render() {
+    return (
+      <div className="popup-verify flex">
+        <p>dsf</p>
+      </div>
+    );
+  }
+}
