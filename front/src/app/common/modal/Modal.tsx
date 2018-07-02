cc5a8f1c52cfc23c4b33a0ca2f400f88eb33a4cb
SlMel <jefferi@ya.ru>
2018-07-02 16:10:06 +0300

    SD-264 Correct modal logic

--- front/src/app/common/modal/Modal.tsx
+++ front/src/app/common/modal/Modal.tsx
@@ -10,11 +10,26 @@ interface IModalProps {
 }
 
 export default class Modal extends React.PureComponent<IModalProps, {}> {
+  private elem: HTMLElement;
+
+  public componentWillMount() {
+    this.elem = document.getElementsByTagName('body')[0];
+  }
+
   public render() {
     const { children, isOpen, onClose } = this.props;
+    const isDisableScroll: boolean = this.elem.classList.contains('modal-disable-scroll');
 
     if (!isOpen) {
+      if (isDisableScroll) {
+        this.elem.classList.remove('modal-disable-scroll');
+      }
+
       return null;
+    }
+
+    if (!isDisableScroll) {
+      this.elem.classList.add('modal-disable-scroll');
     }
 
     return (
