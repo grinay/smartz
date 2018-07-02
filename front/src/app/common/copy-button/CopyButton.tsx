f544e70e3d424bdb977aa88e74cbb52bdd1eb581
SlMel <jefferi@ya.ru>
2018-07-02 16:10:33 +0300

    SD-264 Done copyButton component

--- front/src/app/common/copy-button/CopyButton.tsx
+++ front/src/app/common/copy-button/CopyButton.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 
-import copyImg from '../../../assets/img/common/dapp/copy.svg';
-
 import './CopyButton.less';
 
 
-export default class CopyButton extends React.PureComponent<{}, {}> {
+interface ICopyButtonProps {
+  className?: string;
+  onClick: () => void;
+}
+
+export default class CopyButton extends React.PureComponent<ICopyButtonProps, {}> {
   public render() {
+    const { className, onClick } = this.props;
+
     return (
-      <div className="copy-button">
-        <img src={copyImg} alt="copy" />
+      <div className={`copy-button ${className}`} onClick={onClick}>
+        <img
+          className="img-copy-button"
+          src={require('../../../assets/img/common/components/copy.svg') as string}
+          alt="copy"
+        />
         <span>Copy</span>
       </div>
     );
