56d9594ee4343562f6a6aefc969bef6aeaf12600
quantum13 <quant13@gmail.com>
2018-07-15 21:46:59 +0300

    SD-305 models

--- backend/apps/dapps/models.py
+++ backend/apps/dapps/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.db import models
 from django.db.models import ForeignKey
 
+from apps.common.constants import BLOCKCHAINS, BLOCKCHAIN_ETHEREUM
 from apps.constructors.models import Constructor
 from apps.users.models import User
 
@@ -57,3 +58,51 @@ class Dapp(models.Model):
             self.created_at = datetime.now(pytz.timezone(settings.TIME_ZONE))
 
         return super().save(*args, **kwargs)
+
+
+class Transaction(models.Model):
+    blockchain = models.CharField(choices=BLOCKCHAINS, max_length=50, default=BLOCKCHAIN_ETHEREUM)
+    tx_id = models.CharField(max_length=128)
+
+    execution_datetime = models.DateTimeField()
+    mining_datetime = models.DateTimeField()
+
+    function_name = models.CharField(max_length=255)
+    function_title = models.CharField(max_length=255)
+    function_description = models.CharField(max_length=1000)
+    function_arguments = models.TextField()
+    info = models.TextField()
+    is_success = models.BooleanField()
+    error = models.CharField(max_length=255)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['tx_id', 'blockchain']),
+        ]
+
+    def __str__(self):
+        return self.tx_id
+
+
+class Request(models.Model):
+    blockchain = models.CharField(choices=BLOCKCHAINS, max_length=50, default=BLOCKCHAIN_ETHEREUM)
+
+    execution_datetime = models.DateTimeField()
+    function_name = models.CharField(max_length=255)
+    function_title = models.CharField(max_length=255)
+    function_description = models.CharField(max_length=1000)
+    function_arguments = models.TextField()
+    result = models.TextField()
+
+    is_success = models.BooleanField()
+    error = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.function_name
+
+
+class Log(models.Model):
+    name = models.CharField(max_length=255)
+    data = models.TextField()
+
+    tx = models.ForeignKey(Transaction, on_delete=models.PROTECT, related_name='logs')
