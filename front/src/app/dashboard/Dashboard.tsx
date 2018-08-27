8ea52b2e4e45e48d884ee733e544c2830086ee3c
SlMel <jefferi@ya.ru>
2018-08-27 19:08:59 +0300

    SD-296 Change modal animation time

--- front/src/app/dashboard/Dashboard.tsx
+++ front/src/app/dashboard/Dashboard.tsx
@@ -147,7 +147,7 @@ export default class Dashboard extends React.Component<IDashboardProps, IDashboa
           classNameWindow="dashboard-modal"
           onClose={this.onToggleModal(false)}
           animationWindow={{
-            duration: 3000,
+            duration: 300,
             styleStart: {
               opacity: 0,
               transform: 'scale(.9,.9)',
@@ -158,7 +158,7 @@ export default class Dashboard extends React.Component<IDashboardProps, IDashboa
             },
           }}
           animationBackdrop={{
-            duration: 3000,
+            duration: 300,
             styleStart: { opacity: 0 },
             styleEnd: { opacity: 1 },
           }}
